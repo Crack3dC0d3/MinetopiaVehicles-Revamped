86b084a5ff5fc3ada5f9ceabee3a09c06bd2810a
Crack3dC0d3 <mderuig123@gmail.com>
2020-06-27 22:32:37 +0200

    Refined Quit.java

--- Plugin/src/main/java/me/crack3dc0d3/minetopiavehiclesrevamp/main/events/Quit.java
+++ Plugin/src/main/java/me/crack3dc0d3/minetopiavehiclesrevamp/main/events/Quit.java
@@ -1,5 +1,7 @@
 package me.crack3dc0d3.minetopiavehiclesrevamp.main.events;
 
+import me.crack3dc0d3.minetopiavehiclesrevamp.main.api.vehicle.Seat;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,7 +12,10 @@ public class Quit implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        player.getVehicle().removePassenger(player);
+        if (player.getVehicle() instanceof ArmorStand) {
+            if (Seat.getSeat((ArmorStand) player.getVehicle()) != null) {
+                player.getVehicle().removePassenger(event.getPlayer());
+            }
+        }
     }
-
 }
